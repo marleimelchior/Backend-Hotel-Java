99b03c295aa292c7b3a725e4c8a2fd8423e74803
unknown <you@example.com>
2024-02-28 21:04:11 -0300

    criação da RoomResponse e model Room

--- src/main/java/com/dailycodework/lakesidehotel/model/Room.java
+++ src/main/java/com/dailycodework/lakesidehotel/model/Room.java
@@ -1,14 +1,13 @@
 package com.dailycodework.lakesidehotel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,16 +20,27 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String roomType;
-
     private BigDecimal roomPrice;
-
     private Boolean isBooked = false;
+    @Lob
+    private Blob photo;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if(bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
     }
 }
